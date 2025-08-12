9e9870f92073f73ffe62007a0fb8fd2d1f5ad9f8
Alexandr Dejneka <alexander.deyneka@jamf.com>
2025-08-12 11:19:49 +0200

    [jamf][ubuntu] add ami support for ubuntu
    [jamf][ubuntu] Refactor packer templates based on upstream
    
    [jamf][ubuntu] set volume type to gp3
    
    [jamf][ubuntu] add ami support for ubuntu 24
    
    [jamf][ubuntu] do not install miniconda due to license issues
    
    [jamf][ubuntu] add amazon required plugin
    
    [jamf][ubuntu] refactor script
    
    [jamf][ubuntu] packer fmt
    
    [jamf]ubuntu] add amazon-ebs.ubuntu-base-2204 source
    
    [jamf][ubuntu] waagent specific provisioner to be run only on azure-arm.build_image
    
    [jamf][ubuntu] move azure-arm specific configuration into configure-environment-azure-arm.sh
    
    [jamf][ubuntu] skip read_ahead_kb device check on AWS EC2 (no sd* devices, uses nvme*)
    
    Updating readme file for ubuntu24 version 20250330.1.1

--- images/ubuntu/scripts/tests/Tools.Tests.ps1
+++ images/ubuntu/scripts/tests/Tools.Tests.ps1
@@ -37,7 +37,7 @@ Describe "Rust" {
     It "Rustfmt is installed" {
         "rustfmt --version" | Should -ReturnZeroExitCode
     }
-    
+
     It "cargo" {
         "cargo --version" | Should -ReturnZeroExitCode
     }
@@ -288,12 +288,6 @@ Describe "Kubernetes tools" {
 Describe "Leiningen" -Skip:((-not (Test-IsUbuntu22-X64))) {
     It "leiningen" {
         "lein --version" | Should -ReturnZeroExitCode
-    }
-}
-
-Describe "Conda" -Skip:(-not ((Test-IsUbuntu22-X64) -or (Test-IsUbuntu24-X64))) {
-    It "conda" {
-        "conda --version" | Should -ReturnZeroExitCode
     }
 }
 
